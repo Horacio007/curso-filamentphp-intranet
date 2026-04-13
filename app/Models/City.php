27798d7a46d474d0f014d9a6917131a0519effea
DesarrolloPLS <desarrollo@plasalle.mx>
2026-04-13 16:46:02 -0600

    se crean migraciones de estado y municipio

---
+++ app/Models/City.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+
+    protected $fillable = [
+        'name',
+        'state_code',
+        'city_code',
+        'geo_code',
+        'state_id',
+    ];
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+}
